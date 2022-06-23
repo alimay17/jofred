883ad9df5976613f829ea8b85c9c187ec542987a
Aly Smith <alimay.smith@outlook.com>
2022-06-23 15:35:37 -0500

    updated key

--- jofrid/settings.py
+++ jofrid/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j*ttm#8lju%di(^-s240d(^sesdcme)e*5_2ip&mle_fc1@@l!'
+with open ('.vscode/log.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
