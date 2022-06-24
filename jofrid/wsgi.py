f30534105cb4810f881a7c501cac678016403c66
Aly Smith <alimay.smith@outlook.com>
2022-06-24 13:21:30 -0500

    added base template and css

--- jofrid/wsgi.py
+++ jofrid/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jofrid.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jofrid.settings')
 application = get_wsgi_application()
