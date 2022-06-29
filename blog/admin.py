112178133a12222fb31694a563588b8b7f63ff7e
Aly Smith <alimay.smith@outlook.com>
2022-06-29 16:54:39 -0500

    working on updating functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['title', 'published_date', 'text']}),
+    ]
+  list_display = ('title','created_date','published_date', 'author')
+  list_filter = ['created_date', 'published_date']
+  search_fields = ['title']
+
+
+admin.site.register(Post, BlogAdmin)
